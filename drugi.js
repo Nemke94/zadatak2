398cb11cc22b7e5038ab1de02f850ac970503734
Nemanja Grujić <nemanjagrujic1994@gmail.com>
2017-09-08 01:20:32 +0200

    Add files via upload

---
+++ drugi.js
@@ -0,0 +1,12 @@
+var N = 10;
+var suma = 0;
+var i = 1;
+while( i <= N ) {
+        suma+=i;
+        i++;
+        console.log("Suma N Prirodnih Brojeva : " + suma);
+}   
+
+
+                
+            
